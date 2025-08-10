600b19127393108a876e37abb903ea3bfee6b556
Shubh Kesarwani <zshubh121@gmail.com>
2025-08-10 14:27:02 +0000

    what the fuck i gave you a pillar not a fucking a

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,8 +7,10 @@ const AstraIcon = (props: React.SVGProps<SVGSVGElement>) => (
     fill="currentColor"
     {...props}
   >
-    <path d="M12 2L0 22H4.5L12 7.5L19.5 22H24L12 2ZM12 11.5L10.5 16H13.5L12 11.5Z" />
-    <path d="M12 13.25L11.5 14.5L12 15.75L12.5 14.5L12 13.25Z" />
+    <path d="M5 22H19V20H5V22Z" />
+    <path d="M6 19H18V9H6V19ZM8 17V11H10V17H8ZM11 17V11H13V17H11ZM14 17V11H16V17H14Z" />
+    <path d="M4 8H20V6H4V8Z" />
+    <path d="M3 5H21V3C21 2.44772 20.5523 2 20 2H4C3.44772 2 3 2.44772 3 3V5Z" />
   </svg>
 );
 
